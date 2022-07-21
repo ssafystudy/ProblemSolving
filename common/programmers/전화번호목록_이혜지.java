e37c212b2696b30b356d68cac3e969e649ef5a37
Hyeji Lee <zzambbang@gmail.com>
2022-07-21 20:48:12 +0900

    leehyeji319:: common/programmers 전화번호 목록 수정
    * zzambbang::common/programmers 전화번호목록.java 추가
    
    * leehyeji319:: 전화번호 목록 수정

---
+++ common/programmers/전화번호목록_이혜지.java
@@ -0,0 +1,39 @@
+package common.programmers;
+
+//프로그래머스 해시 lv2 전화번호목록
+import java.util.HashMap;
+class 전화번호목록_이혜지 {
+
+    public static void main(String[] args) {
+        전화번호목록_이혜지 sol = new 전화번호목록_이혜지();
+        String[] phoneBook = {"119", "97674223", "1195524421"};
+        System.out.println((sol.solution(phoneBook)));
+        
+    }
+
+    public boolean solution(String[] phoneBook) {
+        
+        boolean answer = true;
+
+        //해시맵으로 처리
+        HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
+
+        for (int i = 0; i < phoneBook.length; i++) {
+            hashMap.put(phoneBook[i], i);
+        }
+
+        for (int i = 0; i <phoneBook.length; i++) {
+            String phoneNum = "";
+            for (int j = 0; j < phoneBook[i].length(); j++) { 
+                //전화번호 슬라이싱
+                phoneNum += Character.toString(phoneBook[i].charAt(j));
+                //해시맵 번호 비교 
+                if (hashMap.get(phoneNum) != null && !phoneNum.equals(phoneBook[i]))
+                answer = false;
+                return answer;
+            }
+        }
+        
+        return answer;
+    }
+}
