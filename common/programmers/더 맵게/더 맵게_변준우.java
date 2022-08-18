6ad79b7ac8ef0ef74b9c3b1da20f08f85a961543
Junewoo Byeon <movebxeax@gmail.com>
2022-08-18 20:53:04 +0900

    movebxeax:: programmers 더 맵게_변준우.java 수정

--- common/programmers/더 맵게/더 맵게_변준우.java
+++ common/programmers/더 맵게/더 맵게_변준우.java
@@ -3,24 +3,21 @@ import java.util.PriorityQueue;
 class Solution {
     public static int solution(int[] scoville, int K) {
 		int answer = 0;
-        
+
 		PriorityQueue<Integer> pq = new PriorityQueue<>();
 
 		for(int itr : scoville)
 			pq.offer(itr);
-        
-		while(!pq.isEmpty() && pq.size() >= 2)
+
+		while(pq.size() >= 2)
 		{
 			if(pq.peek() >= K)
 				break;
-			
-			int first = pq.poll();
-			int second = pq.poll();
-            
-			pq.offer(first + (second * 2));
+
+			pq.offer(pq.poll() + (pq.poll() * 2));
 			answer++;
 		}
-		
+
 		if(pq.peek() < K)
 			return -1;
 
