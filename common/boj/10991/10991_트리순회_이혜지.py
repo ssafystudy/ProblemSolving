2e9332df50197f261ed76a9daa994d3155a15a84
Hyeji Lee <zzambbang@gmail.com>
2022-10-21 00:02:13 +0900

    leehyeji319:: boj 10991_트리순회_이혜지.py 추가

---
+++ common/boj/10991/10991_트리순회_이혜지.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self, data, left_node, right_node):
+        self.data = data
+        self.left_node = left_node
+        self.right_node = right_node
+
+
+def pre_order(node):
+    print(node.data, end='')
+    if node.left_node != None:
+        pre_order(tree[node.left_node])
+    if node.right_node != None:
+        pre_order(tree[node.right_node])
+
+def in_order(node):
+    if node.left_node != None:
+        in_order(tree[node.left_node])
+    print(node.data, end='')
+    if node.right_node != None:
+        in_order(tree[node.right_node])
+
+def post_order(node):
+    if node.left_node != None:
+        post_order(tree[node.left_node])
+    if node.right_node != None:
+        post_order(tree[node.right_node])
+    print(node.data, end='')
+
+
+
+N = int(input())
+tree = {}
+for _ in range(N):
+    data, left_node, right_node = input().split()
+    if left_node == ".":
+        left_node = None
+    if right_node == ".":
+        right_node = None
+    tree[data] = Node(data, left_node, right_node)
+
+
+#전위 중위 후휘 순으로
+pre_order(tree['A'])
+print()
+in_order(tree['A'])
+print()
+post_order(tree['A'])
