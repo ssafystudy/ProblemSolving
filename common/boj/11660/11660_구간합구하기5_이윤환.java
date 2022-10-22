facfaaebc9d89ae90c2f07d2492ed19b7ae523ab
LeeYunHwan <woooia1@gmail.com>
2022-10-22 13:25:55 +0900

    unannn:: boj 11660_구간합구하기5_이윤환.java 추가

---
+++ common/boj/11660/11660_구간합구하기5_이윤환.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] table = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                table[i][j] = Integer.parseInt(st.nextToken()) + table[i][j - 1];
+            }
+        }
+
+        int[][] sumTable = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                sumTable[j][i] = table[j][i] + sumTable[j - 1][i];
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            sb.append(sumTable[x2][y2] - sumTable[x2][y1 - 1] - sumTable[x1 - 1][y2] + sumTable[x1 - 1][y1 - 1]).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
